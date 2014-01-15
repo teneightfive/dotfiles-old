b959361a61bef4a70ba5c2338f00823e0a5598a9
Dom Smith <dom@teneightfive.com>
2014-01-15 17:24:58 +0000

    Add sync of Sublime files

--- sync.sh
+++ sync.sh
@@ -1,27 +1,52 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE}")"
 git pull
-function doIt() {
+function syncDot() {
   rsync --exclude "init/" \
         --exclude ".git/" \
         --exclude ".DS_Store" \
-        --exclude ".cask" \
         --exclude ".brew" \
-        --exclude ".osx" \
+        --exclude ".cask" \
         --exclude ".install-deps.sh" \
-        --exclude "sync.sh" \
-        --exclude "README.md" \
+        --exclude ".osx" \
         --exclude "Brewfile" \
+        --exclude "README.md" \
+        --exclude "sync.sh" \
         -av . ~
 }
+function syncSt() {
+  rsync --exclude ".git/" \
+        --exclude ".DS_Store" \
+        --exclude ".aliases" \
+        --exclude ".bash_profile" \
+        --exclude ".bash_prompt" \
+        --exclude ".brew" \
+        --exclude ".cask" \
+        --exclude ".exports" \
+        --exclude ".functions" \
+        --exclude ".git-completion.bash" \
+        --exclude ".gitconfig" \
+        --exclude ".gitignore" \
+        --exclude ".install-deps.sh" \
+        --exclude ".osx" \
+        --exclude ".path" \
+        --exclude "domsmith.itermcolors" \
+        --exclude "Brewfile" \
+        --exclude "README.md" \
+        --exclude "sync.sh" \
+        -av ./init/ ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+}
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-  doIt
+  syncDot
+  syncSt
 else
   read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
   echo
   if [[ $REPLY =~ ^[Yy]$ ]]; then
-    doIt
+    syncDot
+    syncSt
   fi
 fi
-unset doIt
+unset syncDot
+unset syncSt
 source ~/.bash_profile
